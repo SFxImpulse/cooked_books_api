4d4693129a347883d2c6764bc823b4de49be9a26
David Braverman <braverman.david.business@gmail.com>
2022-11-10 11:47:28 -0500

    refactor favourite setting query.

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -9,34 +9,21 @@ module.exports = (db, updateFavourites) => {
 
   router.put("/recipes/:id", (req, res) => {
 
-    const { favourite } = req.body.favourite;
+    const favourite = req.body.favourite;
     
-    console.log(req.body);
+    console.log(req.body.favourite);
     console.log(req.params);
 
-    if (!favourite) {
-      db.query(`UPDATE recipes SET favourite = true WHERE id = $1::integer`,
-        [req.params.id]
-      )
-        .then(() => {
-          setTimeout(() => {
-            res.status(204).json({});
-            updateFavourites(req.params.id);
-          }, 1000)
-        })
-        .catch(error => console.log(error));
-    } else {
-      db.query(`UPDATE recipes SET favourite = false WHERE id = $1::integer`,
-        [req.params.id]
-      )
-        .then(() => {
-          setTimeout(() => {
-            res.status(204).json({});
-            updateFavourites(req.params.id);
-          }, 1000)
-        })
-        .catch(error => console.log(error));
-    }
+    db.query(`UPDATE recipes SET favourite = $1::boolean WHERE id = $2::integer`,
+      [favourite, Number(req.params.id)]
+    )
+      .then(() => {
+        setTimeout(() => {
+          res.status(204).json({});
+          updateFavourites(req.params.id);
+        }, 1000)
+      })
+      .catch(error => console.log(error));
   });
 
   return router;
